3ce13321e781e576ef1b6d130445247f4d5840a5
ztzhu1 <zhuzitian@zju.edu.cn>
2022-12-05 10:34:59 +0800

    Support fixed point arithmetic

---
+++ src/threads/fixed_point.h
@@ -0,0 +1,22 @@
+#ifndef __FIXED_POINT_H
+#define __FIXED_POINT_H
+
+#include <stdint.h>
+
+// #define P 17
+// #define Q 14
+#define F 16384 // 2**Q
+
+#define INT_TO_FP(n) ((n) * F)
+#define FP_TO_INT_Z(x) ((x) / F) // rounding toward zero
+#define FP_TO_INT_N(x) ((x) >= 0 ? ((x) + F / 2) / F : ((x) - F / 2) / F) // rounding to nearest
+#define ADD_FP_FP(x, y) ((x) + (y))
+#define SUB_FP_FP(x, y) ((x) - (y))
+#define ADD_FP_INT(x, n) ((x) + INT_TO_FP(n))
+#define SUB_FP_INT(x, n) ((x) - INT_TO_FP(n))
+#define MUL_FP_FP(x, y) (((int64_t)(x)) * (y) / F)
+#define MUL_FP_INT(x, n) ((x) * (n))
+#define DIV_FP_FP(x, y) (((int64_t)(x)) * F / (y))
+#define DIV_FP_INT(x, n) ((x) / (n))
+
+#endif
