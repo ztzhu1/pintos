2d002669e4159ffda02111bddaea648ca7492c99
ztzhu1 <zhuzitian@zju.edu.cn>
2022-12-06 12:24:27 +0800

    Assert interuption level first

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -100,12 +100,13 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
+  ASSERT (intr_get_level () == INTR_ON);
+
   if (ticks < 0)
     return;
 
   // int64_t start = timer_ticks ();
 
-  ASSERT (intr_get_level () == INTR_ON);
   // while (timer_elapsed (start) < ticks) 
   //   thread_yield ();
 
